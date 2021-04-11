a907f695cbbd95b369257409e54a4ff64ae3345b
pocketknife <reynaldlawrence@outlook.ph>
2021-04-11 11:05:56 +0800

    initial commit

---
+++ SpecFlowProject_livingDoc/Features/Home.feature
@@ -0,0 +1,56 @@
+﻿Feature: Home 
+
+Background: 
+	Given I navigate to URl
+	And I click to Input form
+
+#Scenario: Open Simple form demo
+#	Given I click to Simple demo form
+#	Then : I can see Simple demo form
+
+#Scenario: : Show and verify message
+#	Given : I click to Simple demo form
+#	And I enter "Yow What$$$uppp!!" in input
+#	When I click to Show Message Button
+#	Then : I can see "Yow What$$$uppp!!"
+
+#Scenario Outline: Login as Admin
+#Given I Navigate to the login page
+#And I Enter <username> and <password>
+#When I click to login button
+#Then I can see admin is able to login
+#Examples:
+#| Username       | Pass@123 |
+#| Asd@gmail.com  | Pass@123 |
+#| User@gmail.com | Pass@123 |
+#| Div@gmail.com  | Pass@123 |
+
+#Scenario Outline: Fill the Input form
+#Given User is on the input form Page
+#When User enter <Msg> in input
+#And User click to Show button
+#Then User can see the text
+#Examples: 
+#| Msg                |
+#| Yow What$$$uppp!?! |
+#| Codebun |
+#| CodeDec |
+
+
+	#Examnples of Tables in Specflow
+#Scenario Outline: Fill the Input form using Tables
+#Given User is on the input form Page
+#When User enter Msg in input
+#| Msg                | msg2            |
+#| Yow What$$$uppp!?! | How$ iT Goin!?! |
+#| Codebun            | CodeBun         |
+#| CodeDec            | CodeDec         |
+#And User click to Show button
+#Then User can see the text
+
+#Examnple of Param in Specflow
+Scenario Outline: Fill the Input form using Param
+Given User is on the input form Page
+When User enter "Yow Wh@t$$$upp!?" in input
+And User click to Show button
+Then User can see the text
---
+++ SpecFlowProject_livingDoc/Steps/HomeStepsDefinitions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SpecFlowProject_livingDoc.Steps
+{
+    [Binding]
+    public sealed class HomeStepsDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        IWebDriver Driver = new ChromeDriver();
+        private readonly ScenarioContext _scenarioContext;
+
+        [Given(@"I navigate to URl")]
+        public void GivenINavigateToURl()
+        {
+            Driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/");
+        }
+
+        [Given(@"I click to Input form")]
+        public void GivenIClickToInputForm()
+        {
+            Driver.FindElement(By.XPath("//*[@id='navbar-brand-centered']/ul[1]/li[1]/a")).Click();
+        }
+
+        [Given(@"User is on the input form Page")]
+        public void GivenUserIsOnTheInputFormPage()
+        {
+            Task.Delay(5000).Wait();
+            Console.WriteLine("User is on the page");
+            Driver.FindElement(By.XPath("//*[@id='at-cv-lightbox-close']")).Click();
+            Driver.FindElement(By.XPath("//*[@id='navbar-brand-centered']/ul[1]/li[1]/ul/li[1]/a")).Click();
+        }
+
+
+        [When(@"User enter ""(.*)"" in input")]
+        public void WhenUserEnterInInput(string msg)
+        {
+            ScenarioContext.Current.Add("msg", msg);
+            Driver.FindElement(By.XPath("//*[@id='user-message']")).SendKeys(msg);
+        }
+
+        [When(@"User click to Show button")]
+        public void WhenUserClickToShowButton()
+        {
+            Driver.FindElement(By.XPath("//*[@id='get-input']/button")).Click();
+
+        }
+
+        [Then(@"User can see the text")]
+        public void ThenUserCanSeeTheText()
+        {
+            string msg = ScenarioContext.Current["msg"].ToString();
+            string browsertext = Driver.FindElement(By.XPath("//*[@id='display']")).Text.ToString();
+            Console.WriteLine("msg coming form the above step : " + msg);
+            Assert.IsTrue(msg.Contains(browsertext));
+            //Task.Delay(5000).Wait();
+        }
+    }
+}
